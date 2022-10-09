45e825cd52f21b0a1bae1cfae4abf47389f6c11b
2rlo <jey0776283@gmail.com>
2022-10-09 17:33:35 +0900

    [04장] ELements

--- 조은영/my-app/src/index.js
+++ 조은영/my-app/src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './section3/Library';
+import Clock from './section4/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+setInterval(()=>{
+  root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>
-);
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
