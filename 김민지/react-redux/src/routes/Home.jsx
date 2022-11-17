fa68e7e2d4ca9c9c003ccdf0b6d7f8e0e4604a99
KIM MINJI <sml07113@gmail.com>
2022-11-17 23:58:32 +0900

    [03장] mapStateToProps

--- 김민지/react-redux/src/routes/Home.jsx
+++ 김민지/react-redux/src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
 
   function onChange(e) {
@@ -19,9 +20,16 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      {/* store로부터 state 가져오기 */}
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+// Store와 Component를 연결
+function mapStateToProps(state, ownProps) {
+  return { toDos: state }; // props에 추가
+}
+
+// connect(): Home으로 보내는 props에 추가될 수 있도록 허용
+export default connect(mapStateToProps)(Home);
