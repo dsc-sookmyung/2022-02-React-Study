2f3cc12a64f8263d11c9206552f05bd94bf00562
2rlo <jey0776283@gmail.com>
2022-11-20 23:56:54 +0900

    [04장]  Redux Toolkit

--- 조은영/vanilla-redux/src/components/ToDo.js
+++ 조은영/vanilla-redux/src/components/ToDo.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import { actionCreators } from "../routes/store";
+import { remove } from "../routes/store";
 import { Link } from "react-router-dom";
 
 function ToDo({ text, onBtnClick, id }) {
-    return (<li>
-        <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>
-            DEL
-        </button>
-        </Link>
+  return (
+    <li>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
-    );
+  );
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
-    return {
-        onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
-    };
+  return {
+    onBtnClick: () => dispatch(remove(ownProps.id)),
+  };
 }
 
 export default connect(null, mapDispatchToProps)(ToDo);
