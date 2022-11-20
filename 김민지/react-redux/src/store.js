4724f33aff9cb4756e0fdf71d4c6784805912e98
KIM MINJI <sml07113@gmail.com>
2022-11-20 17:40:03 +0900

    [04장] createReducer

--- 김민지/react-redux/src/store.js
+++ 김민지/react-redux/src/store.js
@@ -1,32 +1,18 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
-// const addToDo = (text) => {
-//   return {
-//     type: ADD,
-//     text,
-//   };
-// };
-
 const deleteToDo = createAction("DELETE");
-// const deleteToDo = (id) => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id),
-//   };
-// };
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type: // ADD
-      return [{ text: action.payload, id: Date.now() }, ...state]; // 기존 배열에 직접 값을 추가하지 말기! state is immutable(변경 불가)
-    case deleteToDo.type: // DELETE
-      return state.filter((toDo) => toDo.id !== action.payload); // 삭제하고자 하는 id를 갖고 있지 않은 배열만 필터링
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) => {
+      state.filter((toDo) => toDo.id !== action.payload); // 삭제하고자 하는 id를 갖고 있지 않은 배열만 필터링
+    });
+});
 
 const store = createStore(reducer);
 
