ce3e48989ddd5c974f0be37687cc8f5d19ae91eb
KIM MINJI <sml07113@gmail.com>
2022-11-20 14:32:46 +0900

    [03장] Detail Screen

--- 김민지/react-redux/src/routes/Detail.jsx
+++ 김민지/react-redux/src/routes/Detail.jsx
@@ -1,5 +1,21 @@
-function Detail() {
-  return "Detail";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+  const { id } = useParams();
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+
+  return (
+    <div>
+      <h1>{toDo.text}</h1>
+      <h5>created at: {toDo.id}</h5>
+    </div>
+  );
 }
 
-export default Detail;
+function mapStateToProps(state) {
+  // 주의: react-router-dom 6버전부터는 더이상 history, location, match 등의 props를 component에서 받을 수 없다.
+  return { toDos: state }; // props에 추가
+}
+
+export default connect(mapStateToProps)(Detail);
