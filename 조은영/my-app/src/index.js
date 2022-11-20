b18d9a3c341c78ae28ed41dc339eb2c5eedb741f
2rlo <jey0776283@gmail.com>
2022-11-20 22:59:30 +0900

    [11장]  Forms

--- 조은영/my-app/src/index.js
+++ 조은영/my-app/src/index.js
@@ -10,11 +10,12 @@ import NotificationList from './section06/NotificationList';
 import AttendanceBook from './section10/AttendanceBook';
 import Accommodate from './section07/Accommodate';
 import ConfirmButton from './section08/ConfirmButton';
+import SignUp from './section11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <SignUp />
   </React.StrictMode>
   );
 
